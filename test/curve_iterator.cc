40d4620be2dd54c0285c491a69800df9eaa2d72d
Martin Westerholt-Raum <martin@raum-brothers.eu>
2016-04-13 20:25:38 +0200

    Extend curve iterator testing

--- test/curve_iterator.cc
+++ test/curve_iterator.cc
@@ -45,3 +45,40 @@ BOOST_AUTO_TEST_CASE( enumerate_f2_g1 )
   if ( positions_deg3 != positions_deg3_valid )
     message_positions("genus 1 degree 3 curves / F_2: ", positions_deg3);
 }
+
+
+BOOST_AUTO_TEST_CASE( enumerate_f3_g0 )
+{
+  FqElementTable table(3, 1);
+  CurveIterator iter(table, 0, 1);
+
+  vector<vector<int>> positions_deg2;
+  vector<vector<int>> positions_deg1;
+  for (; !iter.is_end(); iter.step() ) {
+    auto position = iter.as_position();
+    for_each( position.begin(), position.end(),
+              [&table](int &p){ p = table.at_nmod(p); } );
+    if ( position.size() == 3 )
+      positions_deg2.emplace_back(position);
+    else
+      positions_deg1.emplace_back(position);
+  }
+  sort(positions_deg2.begin(), positions_deg2.end());
+  sort(positions_deg1.begin(), positions_deg1.end());
+
+
+  vector<vector<int>> positions_deg2_valid = 
+       { {0,0,1}, {0,0,2}
+       , {1,0,1}, {1,0,2}
+       , {2,0,1}, {2,0,2}
+       };
+  if ( positions_deg2 != positions_deg2_valid )
+    message_positions("genus 1 degree 2 curves / F_3: ", positions_deg2);
+
+  vector<vector<int>> positions_deg1_valid = 
+       { {0,1}, {0,2}
+       };
+  if ( positions_deg1 != positions_deg1_valid )
+    message_positions("genus 1 degree 1 curves / F_3: ", positions_deg1);
+}
+
