65424ec846626da2b3e2e0e1adc84bfc203ba68d
Martin Westerholt-Raum <martin@raum-brothers.eu>
2016-04-14 00:54:53 +0200

    Add licence to every file

--- src/reduction_table.cc
+++ src/reduction_table.cc
@@ -1,3 +1,26 @@
+/*============================================================================
+
+    (C) 2016 Martin Westerholt-Raum
+
+    This file is part of HyCu.
+
+    HyCu is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    HyCu is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HyCu; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+
+
 #include <cmath>
 #include <flint/fq_nmod.h>
 #include <flint/fmpz.h>
